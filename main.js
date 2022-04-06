9e8557109061ace4aa5292a87c02e5bd4780b2c7
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-04-06 20:50:57 +0200

    init (and hopefully last version)

---
+++ main.js
@@ -0,0 +1,32 @@
+export const updateParsedHtml = (htmlList, pack) => {
+  for (let i in htmlList) {
+    if (htmlList[i].type != "html") continue;
+    updateParsedHtml(htmlList[i].innerHTML, pack);
+    let componentIndex = Object.keys(pack.components).indexOf(htmlList[i].tag);
+    if (componentIndex <= -1) continue;
+    htmlList[i] = {
+      type: "html",
+      tag: "div",
+      innerHTML: htmlList[i].innerHTML,
+      attributes: [
+        { attribute: "class", value: "component" },
+        { attribute: "src", value: pack.components[htmlList[i].tag].src },
+        ...htmlList[i].attributes,
+      ],
+    };
+  }
+};
+
+export const updateHtml = (html, Parser, pack) => {
+  let htmlParser = new Parser();
+  for (let i in pack.components) {
+    if (!pack.components[i].needsClosingTag)
+      htmlParser.selfClosingTags.push(i.toLowerCase());
+  }
+
+  let parsedHtml = htmlParser.parse(html);
+
+  updateParsedHtml(parsedHtml, pack);
+
+  return parsedHtml;
+};
