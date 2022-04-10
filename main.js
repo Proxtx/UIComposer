16371df4499bcc1e1cb89d4c1cecaef6f0b9031f
Proxtx <39201896+Proxtx@users.noreply.github.com>
2022-04-10 17:50:03 +0200

    reworked the entire system

--- main.js
+++ main.js
@@ -1,7 +1,48 @@
-export const updateParsedHtml = (htmlList, pack) => {
+export const updateParsedHtml = async (htmlList, pack, build, load, Parser) => {
+  supplementParsedHtml(htmlList, pack);
+  let buildHtml = build(htmlList);
+  let wrap = document.createElement("div");
+  for (let i of buildHtml) wrap.appendChild(i);
+  for (let t of wrap.getElementsByClassName("component")) {
+    let attributes = {};
+    t.getAttributeNames().forEach(
+      (value) => (attributes[value] = t.getAttribute(value))
+    );
+    let component = await load(t.getAttribute("src"))(attributes);
+    component.load().then(async () => {
+      let updatedHtml = await updateHtml(
+        component.element.contentDocument.body.innerHTML,
+        Parser,
+        pack,
+        build,
+        load
+      );
+      for (let i of component.element.contentDocument.body.children) {
+        component.element.contentDocument.body.removeChild(i);
+      }
+      component.element.contentDocument.body.appendChild(updatedHtml);
+    });
+    component.load().then(
+      () =>
+        (component.element.contentWindow.composer = {
+          updateParsedHtml,
+          updateHtml,
+          pack,
+          build,
+          load,
+          Parser,
+        })
+    );
+    t.parentElement.replaceChild(component.element, t);
+  }
+
+  return wrap;
+};
+
+const supplementParsedHtml = (htmlList, pack) => {
   for (let i in htmlList) {
     if (htmlList[i].type != "html") continue;
-    updateParsedHtml(htmlList[i].innerHTML, pack);
+    supplementParsedHtml(htmlList[i].innerHTML, pack);
     let componentIndex = Object.keys(pack.components).indexOf(htmlList[i].tag);
     if (componentIndex <= -1) continue;
     htmlList[i] = {
@@ -22,7 +63,7 @@ export const updateParsedHtml = (htmlList, pack) => {
   }
 };
 
-export const updateHtml = (html, Parser, pack) => {
+export const updateHtml = async (html, Parser, pack, build, load) => {
   let htmlParser = new Parser();
   for (let i in pack.components) {
     if (!pack.components[i].needsClosingTag)
@@ -31,7 +72,7 @@ export const updateHtml = (html, Parser, pack) => {
 
   let parsedHtml = htmlParser.parse(html);
 
-  updateParsedHtml(parsedHtml, pack);
+  let buildHtml = await updateParsedHtml(parsedHtml, pack, build, load, Parser);
 
-  return parsedHtml;
+  return buildHtml;
 };
